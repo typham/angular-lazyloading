ca5132a6c4df04727fe86562865b621c8d1e3579
ty.pham <phamty113@gmail.com>
2018-09-05 17:13:33 +0700

    update

---
+++ src/app/orders/orders-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { OrderListComponent } from './order-list/order-list.component';
+
+const routes: Routes = [
+  {
+    path: 'list',
+    component: OrderListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OrdersRoutingModule { }
